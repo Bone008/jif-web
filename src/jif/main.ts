e93c1b7aadf4ad3177c16f3033283df1114fdd19
Lukas <Bone008@users.noreply.github.com>
2024-12-26 22:38:50 +0100

    prettier format JIF code

--- src/jif/main.ts
+++ src/jif/main.ts
@@ -1,12 +1,16 @@
 import { FullJIF, FullThrow, loadWithDefaults } from "./jif_loader";
-import { addManipulator, formatManipulator, ManipulatorInstruction } from "./manipulation";
+import {
+  addManipulator,
+  formatManipulator,
+  ManipulatorInstruction,
+} from "./manipulation";
 import orbits from "./orbits";
 import * as testdata from "./test_data";
 
 function main(args: string[]) {
-  const verbose = args.includes('-v');
-  const withManipulation = args.includes('-m');
-  const iterateAll = args.includes('-A');
+  const verbose = args.includes("-v");
+  const withManipulation = args.includes("-m");
+  const iterateAll = args.includes("-A");
 
   if (iterateAll) {
     doIterateAll();
@@ -53,34 +57,38 @@ function main(args: string[]) {
 
     // Choptopus
     manipulator = [
-      { type: 'substitute', throwTime: 1, throwFromJuggler: 1 },
-      { type: 'intercept2b', throwTime: 3, throwFromJuggler: 2 },
+      { type: "substitute", throwTime: 1, throwFromJuggler: 1 },
+      { type: "intercept2b", throwTime: 3, throwFromJuggler: 2 },
     ];
 
-    console.log('Using manipulation:\n    ' + formatManipulator(data, manipulator));
+    console.log(
+      "Using manipulation:\n    " + formatManipulator(data, manipulator),
+    );
     console.log();
     data = addManipulator(data, manipulator);
-  }
-  else {
-    console.log('#######################################################');
-    console.log('NOT adding any manipulation. Use the -m flag to change.');
-    console.log('#######################################################\n');
+  } else {
+    console.log("#######################################################");
+    console.log("NOT adding any manipulation. Use the -m flag to change.");
+    console.log("#######################################################\n");
   }
 
   if (verbose) {
-    console.log('## INPUT ##');
+    console.log("## INPUT ##");
     console.log(input);
     console.log();
-    console.log('## WITH DEFAULTS ##');
+    console.log("## WITH DEFAULTS ##");
     console.log(data);
     console.log();
-    console.log('Throws with labels:');
+    console.log("Throws with labels:");
     for (const thrw of data.throws) {
       const fromLimb = data.limbs[thrw.from];
-      const fromStr = data.jugglers[fromLimb.juggler].label + ':' + fromLimb.label;
+      const fromStr =
+        data.jugglers[fromLimb.juggler].label + ":" + fromLimb.label;
       const toLimb = data.limbs[thrw.to];
-      const toStr = data.jugglers[toLimb.juggler].label + ':' + toLimb.label;
-      console.log(`- t=${thrw.time}: ${thrw.duration} from ${fromStr} to ${toStr}`);
+      const toStr = data.jugglers[toLimb.juggler].label + ":" + toLimb.label;
+      console.log(
+        `- t=${thrw.time}: ${thrw.duration} from ${fromStr} to ${toStr}`,
+      );
     }
     console.log();
   }
@@ -96,13 +104,21 @@ function doIterateAll() {
     for (const interceptPerson of [0, 1, 2]) {
       for (const substitutePerson of [0, 1, 2]) {
         const spec: ManipulatorInstruction[] = [
-          { type: 'intercept2b', throwTime: interceptTime, throwFromJuggler: interceptPerson },
-          { type: 'substitute', throwTime: (interceptTime + 4) % 6, throwFromJuggler: substitutePerson },
+          {
+            type: "intercept2b",
+            throwTime: interceptTime,
+            throwFromJuggler: interceptPerson,
+          },
+          {
+            type: "substitute",
+            throwTime: (interceptTime + 4) % 6,
+            throwFromJuggler: substitutePerson,
+          },
         ];
         const manipData = addManipulator(data, spec);
         const os = orbits(manipData, false);
         for (const orbit of os) {
-          if (orbit.every(thrw => thrw.duration < 3 || thrw.isManipulated)) {
+          if (orbit.every((thrw) => thrw.duration < 3 || thrw.isManipulated)) {
             goodResults.push([spec, manipData, orbit]);
           }
         }
@@ -113,10 +129,14 @@ function doIterateAll() {
   for (const [spec, jif, orbit] of goodResults) {
     console.log(
       formatManipulator(data, spec, true),
-      '|',
+      "|",
       orbit
-        .map(thrw => `${jif.jugglers[jif.limbs[thrw.from].juggler].label}${thrw.time}(${thrw.duration.toString(36)})`)
-        .join(' --> '));
+        .map(
+          (thrw) =>
+            `${jif.jugglers[jif.limbs[thrw.from].juggler].label}${thrw.time}(${thrw.duration.toString(36)})`,
+        )
+        .join(" --> "),
+    );
   }
 }
 
