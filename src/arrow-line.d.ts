9e17e0aa2132d063ba8fb1b48324d1d11ec90d71
Lukas <Bone008@users.noreply.github.com>
2024-12-27 14:18:50 +0100

    add arrow rendering capabilities using arrow-line

---
+++ src/arrow-line.d.ts
@@ -0,0 +1,31 @@
+// Manual typings provided for the arrow-line module:
+// https://github.com/stanko-arbutina/arrow-line/tree
+
+declare module "arrow-line" {
+  export type ArrowPosition =
+    | "topLeft"
+    | "topRight"
+    | "topCenter"
+    | "middleRight"
+    | "middleLeft"
+    | "bottomLeft"
+    | "bottomCenter"
+    | "bottomRight";
+
+  export interface ArrowLineOptions {
+    source: string | { x: number; y: number };
+    destination: string | { x: number; y: number };
+    sourcePosition?: ArrowPosition;
+    destinationPosition?: ArrowPosition;
+    svgParentSelector?: string;
+    color?: string;
+    thickness?: number;
+    curvature?: number;
+    // Note: Not complete!
+  }
+
+  export default function arrowLine(options: ArrowLineOptions): {
+    remove(): void;
+    update(options: Partial<Omit<ArrowLineOptions, "svgParentSelector">>);
+  };
+}
