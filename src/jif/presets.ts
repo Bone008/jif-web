936cb0e85d74b9404052156559b37659c2881f1b
Lukas Bonauer <bonauer@proximafusion.com>
2025-01-03 17:54:14 +0100

    more presets

--- src/jif/presets.ts
+++ src/jif/presets.ts
@@ -21,8 +21,30 @@ export const ALL_PRESETS: Preset[] = [
     instructions: "3 3 3 3 3 3 3 3",
     manipulators: ["- - sA"],
   },
+  {
+    name: "Happy Holds",
+    instructions: ["2 2 2 2 2 2", "2 2 2 2 2 2"].join("\n"),
+  },
   { name: "3-count", instructions: RAW_DATA_3_COUNT_PASSING.join("\n") },
   { name: "3-count 2x", instructions: RAW_DATA_3_COUNT_PASSING_2X.join("\n") },
+  {
+    name: "3-count 5x with pass intercept",
+    instructions: [
+      "3B 3  3  3B 3  3  3B 3  3  3B 3  3  3B 3  3",
+      "3A 3  3  3A 3  3  3A 3  3  3A 3  3  3A 3  3",
+    ].join("\n"),
+    manipulators: ["- - - ia"],
+  },
+  {
+    name: "TODO: Dolby 5.1",
+    instructions: ["3B 3  3  3  3", "3A 3  3  3  3"].join("\n"),
+    manipulators: ["sA iiB -  -  -"],
+  },
+  {
+    name: "TODO: Dolby 5.1 with Doppelgänger",
+    instructions: ["3B 3  3  3  3", "3A 3  3  3  3"].join("\n"),
+    manipulators: ["sA iiB -  -  -", "sB iiA -  -  -"],
+  },
   { name: "4-count", instructions: RAW_DATA_4_COUNT_PASSING.join("\n") },
   { name: "4-count 2x", instructions: RAW_DATA_4_COUNT_PASSING_2X.join("\n") },
   {
@@ -44,6 +66,11 @@ export const ALL_PRESETS: Preset[] = [
     instructions: RAW_DATA_WALKING_FEED_9C.join("\n"),
     // Standard notation: ["cB sC iC", "#1.9", "Postmen"],
     manipulators: ["- - sA - iiC"],
+  },
+  {
+    name: "Scrambled - iA cC sC - Ivy",
+    instructions: RAW_DATA_WALKING_FEED_9C.join("\n"),
+    manipulators: ["iiB - - - sC"],
   },
   {
     name: "Walking feed 10c",
@@ -69,5 +96,10 @@ export const ALL_PRESETS: Preset[] = [
   {
     name: "975 - Holy Grail",
     instructions: "975975975",
+  },
+  {
+    name: "TODO: Muckabout",
+    instructions: ["3 3c 3 3 3b 3", "3 3 3 3 3a 3", "3 3a 3 3 3 3"].join("\n"),
+    manipulators: ["- sa - iic - -", "iib - - - sb -"],
   },
 ];
