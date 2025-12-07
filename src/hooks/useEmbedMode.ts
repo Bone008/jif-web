4c1d1059d493cf3041558e88ef2dd1e4a96cad12
Lukas <Bone008@users.noreply.github.com>
2025-12-07 23:04:05 +0100

    include link to open full page from embed page

--- src/hooks/useEmbedMode.ts
+++ src/hooks/useEmbedMode.ts
@@ -7,3 +7,17 @@ export function useEmbedMode(): boolean {
   const search = useSearchParams();
   return search.get(EMBED_PARAM) === "1";
 }
+
+/** Returns the current URL with embed mode enabled. */
+export function getUrlWithEmbedMode() {
+  const url = new URL(location.href);
+  url.searchParams.append(EMBED_PARAM, "1");
+  return url.toString();
+}
+
+/** Returns the current URL with embed mode disabled. */
+export function getUrlWithoutEmbedMode() {
+  const url = new URL(location.href);
+  url.searchParams.delete(EMBED_PARAM);
+  return url.toString();
+}
